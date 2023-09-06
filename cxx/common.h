1b1c1b8ecefc886d0efd9ac81912ca4e2c745688
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-09-06 19:00:50 -0400

    simplify wrapping/unwrapping using alignment

--- cxx/common.h
+++ cxx/common.h
@@ -13,33 +13,18 @@ namespace lean_gccjit
 
 static_assert(sizeof(size_t) >= sizeof(void *), "size_t must be at least as large as a pointer");
 
-constexpr static inline bool POINTER_OF_64BIT = sizeof(size_t) == 8;
-
 template <typename T>
 static inline lean_obj_res wrap_pointer(T * ptr)
 {
     size_t value = reinterpret_cast<size_t>(ptr);
-    if constexpr (POINTER_OF_64BIT)
-    {
-        return lean_box(value);
-    }
-    else
-    {
-        return lean_box_usize(value);
-    }
+    return reinterpret_cast<lean_obj_res>(value | 1);
 }
 
 template <typename T>
 static inline T * unwrap_pointer(b_lean_obj_arg obj)
 {
-    if constexpr (POINTER_OF_64BIT)
-    {
-        return reinterpret_cast<T *>(lean_unbox(obj));
-    }
-    else
-    {
-        return reinterpret_cast<T *>(lean_unbox_usize(obj));
-    }
+    size_t value = reinterpret_cast<size_t>(obj);
+    return reinterpret_cast<T *>(value & (~1));
 }
 
 static inline lean_obj_res lean_option_string(const char * str)
