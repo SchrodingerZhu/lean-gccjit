a9b68a2f92239985a5b62b7ddd90163df5c34a9b
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-09-06 11:01:51 -0400

    feat: context_new_struct_constructor

--- cxx/values.cpp
+++ cxx/values.cpp
@@ -18,6 +18,54 @@ extern "C" LEAN_EXPORT lean_obj_res lean_gcc_jit_context_new_struct_constructor(
     b_lean_obj_arg ctx, b_lean_obj_arg loc, b_lean_obj_arg ty,
     b_lean_obj_arg fields,               /* Option of Array Field */
     b_lean_obj_arg values, lean_object * /* w */
-) {                                      /* TODO */
+) {
+  auto values_len = lean_array_size(values);
+  if (values_len > INT_MAX) {
+    auto error = lean_mk_io_error_invalid_argument(
+        EINVAL, lean_mk_string("too many values"));
+    return lean_io_result_mk_error(error);
+  }
+
+  auto context = unwrap_pointer<gcc_jit_context>(ctx);
+  auto location = unwrap_pointer<gcc_jit_location>(loc);
+  auto type = unwrap_pointer<gcc_jit_type>(ty);
+  auto num_values = static_cast<int>(values_len);
+
+  auto fields_ctor = lean_obj_tag(fields);
+  gcc_jit_rvalue *result;
+  if (fields_ctor == 1) {
+    auto fields_ = lean_ctor_get(fields, 0);
+    auto fields_len = lean_array_size(fields_);
+    if (fields_len != values_len) {
+      auto error = lean_mk_io_error_invalid_argument(
+          EINVAL, lean_mk_string("fields and values have different lengths"));
+      return lean_io_result_mk_error(error);
+    }
+    result = with_allocation<gcc_jit_rvalue *>(values_len, [=](auto *vbuffer) {
+      return with_allocation<gcc_jit_field *>(fields_len, [=](auto *fbuffer) {
+        for (size_t i = 0; i < fields_len; i++) {
+          fbuffer[i] =
+              unwrap_pointer<gcc_jit_field>(lean_to_array(fields_)->m_data[i]);
+        }
+        for (size_t i = 0; i < values_len; i++) {
+          vbuffer[i] =
+              unwrap_pointer<gcc_jit_rvalue>(lean_to_array(values)->m_data[i]);
+        }
+        return gcc_jit_context_new_struct_constructor(
+            context, location, type, fields_len, fbuffer, vbuffer);
+      });
+    });
+  } else {
+    result = with_allocation<gcc_jit_rvalue *>(values_len, [=](auto *vbuffer) {
+      for (size_t i = 0; i < values_len; i++) {
+        vbuffer[i] =
+            unwrap_pointer<gcc_jit_rvalue>(lean_to_array(values)->m_data[i]);
+      }
+      return gcc_jit_context_new_struct_constructor(
+          context, location, type, num_values, nullptr, vbuffer);
+    });
+  }
+  return map_notnull(result, wrap_pointer<gcc_jit_rvalue>,
+                     "failed to create struct constructor");
 }
 } // namespace lean_gccjit
--- lean/LeanGccJit/Unsafe/Values.lean
+++ lean/LeanGccJit/Unsafe/Values.lean
@@ -1,4 +1,8 @@
 import LeanGccJit.Types
 
 @[extern "lean_gcc_jit_context_new_global"]
-opaque Context.newGlobal (ctx : @& Context) (loc : @& Location) (kind: @& GlobalKind) (type : @& JitType) (name : @& String)  : IO Global
+opaque Context.newGlobal (ctx : @& Context) (loc : @& Location) (kind: @& GlobalKind) (type : @& JitType) (name : @& String)  : IO LValue
+
+@[extern "lean_gcc_jit_context_new_struct_constructor"]
+opaque Context.newStructConstructor 
+  (ctx : @& Context) (loc : @& Location) (type : @& JitType) (fields: @& Option (Array Field)) (values: @& Array RValue)  : IO LValue
