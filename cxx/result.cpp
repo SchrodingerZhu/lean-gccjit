53f3c0b45f18269f246f43a0a165dc4ae143951b
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-09-15 21:33:08 -0400

    add ways to call jit function

--- cxx/result.cpp
+++ cxx/result.cpp
@@ -27,4 +27,16 @@ lean_gcc_jit_result_get_global(b_lean_obj_arg res, b_lean_obj_arg name, lean_obj
     return map_notnull(reinterpret_cast<size_t>(addr), lean_box_usize, "failed to get global");
 }
 
+extern "C" LEAN_EXPORT lean_obj_res
+lean_gcc_jit_result_get_function(b_lean_obj_arg res, b_lean_obj_arg name, lean_object * /* w */)
+{
+    auto result = unwrap_pointer<gcc_jit_result>(res);
+    auto fname = lean_string_cstr(name);
+    auto addr = gcc_jit_result_get_code(result, fname);
+    return map_notnull(
+        addr,
+        [](void * func) { return lean_alloc_closure(func, 1, 0); },
+        "failed to get function");
+}
+
 } // namespace lean_gccjit
--- lean/LeanGccJit/Core/Result.lean
+++ lean/LeanGccJit/Core/Result.lean
@@ -8,6 +8,9 @@ opaque Context.compile: @& Context → IO Result
 @[extern "lean_gcc_jit_result_get_code"]
 opaque Result.getCode: @& Result → @& String → IO USize
 
+@[extern "lean_gcc_jit_result_get_function"]
+opaque Result.getFunction! {α : Type} [Nonempty α] : @& Result → @& String → IO α
+
 @[extern "lean_gcc_jit_result_get_global"]
 opaque Result.getGlobal: @& Result → @& String → IO USize
 
