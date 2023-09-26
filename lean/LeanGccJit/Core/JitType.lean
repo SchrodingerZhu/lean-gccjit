95cf47d2aec0f7d769beeb9b2e66cfd5854eac06
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-09-26 13:25:38 -0400

    doc: finish `JitType` doc

--- lean/LeanGccJit/Core/JitType.lean
+++ lean/LeanGccJit/Core/JitType.lean
@@ -60,49 +60,114 @@ Construct a new union type, with the given name and fields.
 @[extern "lean_gcc_jit_context_new_union_type"]
 opaque Context.newUnionType 
   (ctx : @& Context) (location : @& Option Location) (name : @& String) (fields: @& Array Field) : IO JitType
+/--
+Generate a `JitType` for a function pointer with the given return type and parameters.
 
+Each of param_types must be non-`void`; return_type may be `void`.
+-/
 @[extern "lean_gcc_jit_context_new_function_ptr_type"]
 opaque Context.newFunctionPtrType 
   (ctx : @& Context) (location : @& Option Location) (returnType : @& JitType) (params: @& Array JitType) (isVarArg : @& Bool) : IO JitType
 
+/--
+Given non-void type `T`, get type:
+```c
+T __attribute__ ((aligned (ALIGNMENT_IN_BYTES)))
+```
+The alignment must be a power of two.
+-/
 @[extern "lean_gcc_jit_type_get_aligned"]
 opaque JitType.getAligned (ty : @& JitType) (alignment : @& Nat) : IO JitType
 
+/--
+Given type `T`, get type:
+```
+T  __attribute__ ((vector_size (sizeof(T) * num_units))
+```
+`T` must be integral or floating point; num_units must be a power of two.
+
+This can be used to construct a vector type in which operations are applied element-wise. 
+The compiler will automatically use SIMD instructions where possible. 
+
+See also [Vector Extensions](https://gcc.gnu.org/onlinedocs/gcc/Vector-Extensions.html).
+-/
 @[extern "lean_gcc_jit_type_get_vector"]
 opaque JitType.getVector (ty : @& JitType) (length : @& Nat) : IO JitType
 
+/--
+Get the element type of an array type or `none` if it’s not an array.
+-/
 @[extern "lean_gcc_jit_type_dyncast_array"]
 opaque JitType.dynCastArray (ty : @& JitType) : IO (Option JitType)
 
+/--
+Return `true` if the type is a bool.
+-/
 @[extern "lean_gcc_jit_type_is_bool"]
 opaque JitType.isBool (ty : @& JitType) : IO Bool
 
+/--
+Return the function type if it is one or `none`.
+-/
 @[extern "lean_gcc_jit_type_dyncast_function_ptr_type"]
 opaque JitType.dynCastFunctionPtrType (ty : @& JitType) : IO (Option FunctionType)
 
+/--
+Given a function type, return its return type.
+-/
 @[extern "lean_gcc_jit_function_type_get_return_type"]
 opaque FunctionType.getReturnType (ty : @& FunctionType) : IO JitType
 
+/--
+Given a function type, return its number of parameters.
+-/
 @[extern "lean_gcc_jit_function_type_get_param_count"]
 opaque FunctionType.getParamCount (ty : @& FunctionType) : IO Nat
 
+/--
+Given a function type, return the type of the specified parameter.
+-/
 @[extern "lean_gcc_jit_function_type_get_param_type"]
 opaque FunctionType.getParamType (ty : @& FunctionType) (i : @& Nat) : IO JitType
 
+/--
+Return `true` if the type is an integral.
+-/
 @[extern "lean_gcc_jit_type_is_integral"]
 opaque JitType.isIntegral (ty : @& JitType) : IO Bool
 
+/--
+Return the type pointed by the pointer type or `none` if it’s not a pointer.
+-/
 @[extern "lean_gcc_jit_type_is_pointer"]
 opaque JitType.isPointer (ty : @& JitType) : IO (Option JitType)
 
+/--
+Given a type, return a dynamic cast to a vector type or `none`.
+-/
 @[extern "lean_gcc_jit_type_dyncast_vector"]
 opaque JitType.dynCastVector (ty : @& JitType) : IO (Option VectorType)
 
+/--
+Given a type, return a dynamic cast to a struct type or `none`.
+-/
+@[extern "lean_gcc_jit_type_is_struct"]
+opaque JitType.isStruct (ty : @& JitType) : IO (Option JitType)
+
+/--
+Given a vector type, return the number of units it contains.
+-/
 @[extern "lean_gcc_jit_vector_type_get_num_units"]
 opaque VectorType.getNumUnits (ty : @& VectorType) : IO Nat
 
+/--
+Given a vector type, return the type of its elements.
+-/
 @[extern "lean_gcc_jit_vector_type_get_element_type"]
 opaque VectorType.getElementType (ty : @& VectorType) : IO JitType
 
+/--
+Given a type, return the unqualified type, removing `const`, `volatile` and alignment qualifiers.
+-/
 @[extern "lean_gcc_jit_type_unqualified"]
 opaque JitType.unqualified (ty : @& JitType) : IO JitType
